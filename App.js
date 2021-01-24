f0a6f96e3dac8bc469745c76e822164739492b39
egeerdil <erdil5ege@gmail.com>
2021-01-24 23:41:49 +0300

    second commit

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Login from './screens/Login';
 import Feed from './screens/Feed';
-import Deneme4 from './screens/Deneme4';
 import SearchScreen from './screens/SearchScreen';
 
 <Login />;
@@ -31,13 +30,6 @@ function App() {
         <Stack.Screen
           name="SearchScreen"
           component={SearchScreen}
-          options={{
-            title: 'Grid Seach',
-          }}
-        />
-        <Stack.Screen
-          name="Deneme4"
-          component={Deneme4}
           options={{
             title: 'Grid Seach',
           }}
